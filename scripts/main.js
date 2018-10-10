023ffb7e9fa659096a18c563ede5119a585d82c8
mauro <maurofmferrao@gmail.com>
2018-10-10 08:44:19 +0100

    Basic structure in JS ( blocked by now ) and start animation update

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,53 @@
+// Variables
+const animateStopCond = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
+const pages = {
+    work: {
+        selected: false
+    },
+    projects: {
+        selected: false
+    },
+    about: {
+        selected: false
+    }
+};
+
+
+// Functions
+function goTo(pageToGo) {
+
+    console.log('goTo');
+    
+    for(const key in pages) {
+        if(pages.hasOwnProperty(key)) {
+            const page = pages[key];
+            pageTransition(key, (key === pageToGo));
+        }
+    }
+}
+
+function pageTransition(page, transition) {
+
+    console.log('pageTransition');
+    console.log('pageTransition: Animate ' + page + ' -> ' + transition);
+    $('.content-' + page).toggleClass('show', transition);
+    $('#nav-' + page).toggleClass('active', transition);
+    
+
+}
+
 function preloadImages(array) {
-    if (!preloadImages.list) {
+    if(!preloadImages.list) {
         preloadImages.list = [];
     }
 
     var list = preloadImages.list;
 
-    for (var i = 0; i < array.length; i++) {
+    for(var i = 0;i < array.length;i++) {
         var img = new Image();
         img.onload = function() {
             var index = list.indexOf(this);
-            if (index !== -1) {
+            if(index !== -1) {
                 // remove image from the array once it's loaded
                 // for memory consumption reasons
                 list.splice(index, 1);
@@ -20,6 +58,32 @@ function preloadImages(array) {
     }
 }
 
+function start(runIntro) {
+
+    if(runIntro) {
+        $('.intro-anim-container').addClass('show');
+
+        $('.intro-logo').addClass('animate').one(animateStopCond, function(event) {
+            $(this).removeClass('animate');
+            $(this).hide();
+
+            $('.intro-anim-container').removeClass('show').one(animateStopCond, function(event) {
+                $(this).hide();
+            });
+
+            //goTo('work');
+
+            //$('.navbar').addClass('show');
+        });
+    } else {
+        goTo('work');
+
+        $('.navbar').addClass('show');
+    }
+}
+
+
+// Main
 $(document).ready(function() {
 
     preloadImages([
@@ -36,5 +100,5 @@ $(document).ready(function() {
         'assets/animations/frame11.svg'
     ]);
 
-    $('.canvas').addClass('animate');
+    start(true);
 });
